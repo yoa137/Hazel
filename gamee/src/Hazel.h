cd61e62a18413628fdd5e416b53aecd1c0ec5e63
yoa137 <1127525711@qq.com>
2023-03-06 18:36:55 +0800

    setup basic

---
+++ gamee/src/Hazel.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//for application
+#include<stdio.h>
+
+#include "Hazel/Application.h"
+
+
+
+// --entry point-----------------
+
+#include "Hazel/EntryPoint.h"
+// ------------------------------
